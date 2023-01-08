6fd25a2e37860adc021fa92a89209926416cf617
JanBrzozowski <83976408+j-brzoz@users.noreply.github.com>
2023-01-08 19:11:04 +0100

    basic theme change and modal added

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+// -----------------main logic-----------------
 const myLibrary = [];
 
+// book constructor
 function Book(title, author, pages, read) {
     this.title = title;
   this.author = author;
@@ -11,16 +13,18 @@ function Book(title, author, pages, read) {
   };
 }
 
+// adding books to library
 function addBookToLibrary(book) {
     myLibrary.push(book);
 }
 
+// test
 const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, 1);
 addBookToLibrary(theHobbit);
 const theWitcher = new Book("The Witcher", "Sapkowski", 385, 1);
 addBookToLibrary(theWitcher);
 
-
+// displaying books
 for(let i = 0; i < myLibrary.length; i += 1){
     const main = document.querySelector("#main");
     const bookCard = document.createElement("div");
@@ -29,3 +33,29 @@ for(let i = 0; i < myLibrary.length; i += 1){
     main.appendChild(bookCard);
     bookCard.textContent = myLibrary[i].info();
 }
+
+
+// -----------------theme change-----------------
+function setTheme() {
+  const root = document.documentElement;
+  const newTheme = root.className === "dark" ? "light" : "dark";
+  root.className = newTheme;
+  
+  // document.querySelector(".theme-name").textContent = newTheme;
+}
+
+document.querySelector(".themeChange").addEventListener("click", setTheme);
+
+//  -----------------add book modal-----------------
+const addBookModal = document.querySelector("#addBookModal");
+const addBookBtn = document.querySelector("#addBookBtn");
+
+addBookBtn.onclick = function modalToBlock() {
+  addBookModal.style.display = "block";
+};
+
+window.onclick = function modalToNone(event) {
+  if (event.target === addBookModal) {
+    addBookModal.style.display = "none";
+  }
+};
