ad95f43614c321ef083a2e2fd67b86d69d73a7b3
JanBrzozowski <83976408+j-brzoz@users.noreply.github.com>
2023-01-08 23:43:21 +0100

    added form to add books

--- script.js
+++ script.js
@@ -2,37 +2,34 @@
 const myLibrary = [];
 
 // book constructor
-function Book(title, author, pages, read) {
+function Book(title, author, pages, status) {
     this.title = title;
   this.author = author;
   this.pages = pages;
-  this.read = read;
+  this.status = status;
   this.info = function info() {
-    if (read === 0) return `${title} by ${author}, ${pages} pages, not read yet`;
+    if (status !== "on") return `${title} by ${author}, ${pages} pages, not read yet`;
     return `${title} by ${author}, ${pages} pages, read already`;
   };
+}
+
+// displaying books
+function displayLibrary() {
+  const main = document.querySelector("#main");
+  const bookCard = document.createElement("div");
+  bookCard.classList.add("bookCard");
+  bookCard.setAttribute("id", `bookCard${  myLibrary.length}`);
+  main.appendChild(bookCard);
+  bookCard.textContent = myLibrary[myLibrary.length-1].info();
 }
 
 // adding books to library
 function addBookToLibrary(book) {
     myLibrary.push(book);
+    console.log(book);
+    displayLibrary();
 }
 
-// test
-const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, 1);
-addBookToLibrary(theHobbit);
-const theWitcher = new Book("The Witcher", "Sapkowski", 385, 1);
-addBookToLibrary(theWitcher);
-
-// displaying books
-for(let i = 0; i < myLibrary.length; i += 1){
-    const main = document.querySelector("#main");
-    const bookCard = document.createElement("div");
-    bookCard.classList.add("bookCard");
-    bookCard.setAttribute("id", `bookCard${  i}`);
-    main.appendChild(bookCard);
-    bookCard.textContent = myLibrary[i].info();
-}
 
 
 // -----------------theme change-----------------
@@ -43,8 +40,9 @@ function setTheme() {
   
   // document.querySelector(".theme-name").textContent = newTheme;
 }
-
 document.querySelector(".themeChange").addEventListener("click", setTheme);
+
+
 
 //  -----------------add book modal-----------------
 const addBookModal = document.querySelector("#addBookModal");
@@ -59,3 +57,42 @@ window.onclick = function modalToNone(event) {
     addBookModal.style.display = "none";
   }
 };
+
+
+
+//  -----------------adding book from form-----------------
+const formElem = document.querySelector("#newBookForm");
+formElem.addEventListener("submit", (e) => {
+  e.preventDefault();
+  // eslint-disable-next-line no-new
+  new FormData(formElem);
+});
+
+formElem.addEventListener("formdata", (e) => {
+  console.log("test");
+  let bookTitle;
+  let bookAuthor;
+  let bookPages;
+  let bookStatus;
+  let i = 0;
+  const data = e.formData;
+  
+  // eslint-disable-next-line no-restricted-syntax
+  for (const value of data.values()) {
+    if(i === 0)
+      bookTitle = value;
+    else if(i === 1)
+      bookAuthor = value;
+    else if(i === 2)
+      bookPages = value;
+    else if(i === 3)
+      bookStatus = value;
+    else
+      console.log("error");
+    i+=1;
+  };
+  i = 0;
+  const newBook = new Book(bookTitle, bookAuthor, bookPages, bookStatus);
+  addBookToLibrary(newBook);
+})
+
